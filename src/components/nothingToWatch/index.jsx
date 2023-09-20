bbae87976416f8185bb9ea9f61f6767bfb2f779e
Chetan <mr.chetan005@gmail.com>
2023-09-20 16:10:40 +0530

    updated images path

--- src/components/nothingToWatch/index.jsx
+++ src/components/nothingToWatch/index.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable react/prop-types */
 
-
 const NothingToWatch = ({ message, message2 }) => {
     return (
         <div className="text-center ">
             <div className="m-auto sadIcon xl:h-52 xl:w-52 h-20 w-20 overflow-hidden">
-                <img className='w-full object-cover ' src="/src/assets/icons/sadFile.svg" alt="" />
+                <img className='w-full object-cover ' src="/assets/icons/sadFile.svg" alt="" />
             </div>
             <div className='text-xl pt-4 sm:text-3xl text-[#938f97] '>{message}</div>
             <div className='text-xl pt-4 sm:text-3xl text-[#938f97] '>{message2}</div>
--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import './notFound.css';
-import Layout from '../../components/layout';
 
 const NotFound = () => {
     return (
@@ -8,7 +7,7 @@ const NotFound = () => {
             <section className='mx-[7.5%] bg-[rgba(255,255,255,0.1)] flex items-center justify-center text-center min-h-screen'>
                 <div>
                     <div className="m-auto sadIcon xl:h-52 xl:w-52 h-20 w-20 overflow-hidden">
-                        <img className='w-full object-cover ' src="/src/assets/icons/sadFile.svg" alt="" />
+                        <img className='w-full object-cover ' src="/assets/icons/sadFile.svg" alt="" />
                     </div>
                     <div className='mb-4 text-xl sm:text-3xl xl:text-6xl font-semibold  text-[#938f97] '>Oops! No Information available</div>
                     <Link to={'/'} className='text-[#a785ff] text-lg sm:text-xl xl:text-4xl underline'>GO HOME</Link>
