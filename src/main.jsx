f1412ed2567fba89e1e6750055f31cb019a67fed
Chetan <mr.chetan005@gmail.com>
2023-10-08 20:52:08 +0530

    removed analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,6 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
     {/* </React.StrictMode>, */}
-    <Analytics />
   </>
 )
